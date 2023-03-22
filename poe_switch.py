7ce326fbb0d47ea29872705d599d21dc044f0e55
dammaer <advtmb@ya.ru>
2023-03-22 11:35:38 +0300

    fix poe_switch module

--- poe_switch.py
+++ poe_switch.py
@@ -16,7 +16,7 @@ class Switch():
     ports_up = []
 
     def __init__(self):
-        try:
+        if self.check_telnet_connect():
             self.telnet = pexpect.spawn(f"telnet {SWI_IP}", timeout=30,
                                         encoding="utf-8")
             self.telnet.expect("login")
@@ -26,13 +26,33 @@ class Switch():
             self.telnet.expect(self.prompt)
             self.telnet.sendline("terminal length 0")
             self.telnet.expect(self.prompt)
-        except pexpect.exceptions.EOF:
-            return None
+        else:
+            raise SwiFail('Не удается подключиться к коммутатору! '
+                          'Возможно указан\nне тот IP адрес, '
+                          'либо нет доступа по telnet.')
+
+    def check_telnet_connect(self):
+        '''
+        We make a trial connection to the switch, with the
+        specified ip, login and password in settings.ini
+        '''
+        try:
+            telnet = pexpect.spawn(f"telnet {SWI_IP}", timeout=3,
+                                   encoding="utf-8")
+            telnet.expect("login")
+            telnet.sendline(SWI_LOGIN)
+            telnet.expect("[Pp]assword")
+            telnet.sendline(SWI_PASSWD)
+            telnet.expect(self.prompt)
+            telnet.close()
+            return True
+        except (pexpect.exceptions.TIMEOUT, pexpect.exceptions.EOF):
+            return False
 
     def ethernet_status(self):
         self.telnet.sendline('sh int ethernet status')
         match = self.telnet.expect(self._except)
-        if not match:
+        if not match: # Checking when to parse the output
             output = self.telnet.before.replace("\r\n", ";")
             output = re.sub(r'\s+', '&', output)
             for strg in output.split(';'):
@@ -40,8 +60,6 @@ class Switch():
                 if 'UP/UP' in strg and int(port_num) <= int(SWI_MAX_POE_ETH_PORTS):
                     if port_num != SWI_UPLINK:
                         self.ports_up.append(port_num)
-        else:
-            return False
 
     def ports_range(self):
         try:
@@ -91,7 +109,7 @@ class Switch():
         except pexpect.exceptions.EOF:
             raise SwiFail('Режим конфигурации коммутатора используется '
                           'другим пользователем!')
-
+        
     def enable_port(self, port):
         self.enter_conf_mode()
         self.telnet.sendline(f'int ethernet 1/0/{port}')
